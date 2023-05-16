4fcbf7df93c6bad15b79de9933ed588831dd6819
kwoshvick <kwoshvick@gmail.com>
2023-05-16 18:30:11 +0300

    strict type

---
+++ app/Services/ManagerService.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Supermarket;
+use Illuminate\Database\Eloquent\Collection;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class ManagerService
+{
+    private Supermarket $supermarket;
+
+    public function __construct(Supermarket $supermarket)
+    {
+        $this->supermarket = $supermarket;
+    }
+
+    public function store(array $supermarketAttributes): Supermarket
+    {
+        $supermarket = $this->supermarket->newInstance();
+        $supermarket->fill($supermarketAttributes)->save();
+
+        return $supermarket;
+    }
+
+    public function view(Supermarket $supermarket): Supermarket
+    {
+        return $supermarket;
+
+    }
+
+    public function list(): Collection|array
+    {
+        return QueryBuilder::for(Supermarket::class)
+            ->allowedFilters([
+                'name',
+                'location',
+            ])
+            ->defaultSort('name')
+            ->get();
+    }
+
+    public function update(array $supermarketAttributes, Supermarket $supermarket): Supermarket
+    {
+        $supermarket->update($supermarketAttributes);
+
+        return $supermarket;
+    }
+
+}
